4e330b87a0f920850261353ab4f280377fba661c
MTymofii <mill.timofey@gmail.com>
2022-11-19 23:07:22 +0100

    ESLint fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import {MongooseModule} from "@nestjs/mongoose";
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 
 @Module({
